1938ac5bed77b333a968d7e5734e02890c0fcfe3
HaleyOtaku <haleyotaku@gmail.com>
2024-05-27 11:53:11 -0500

    Add project files.

---
+++ C#_Rock_Paper_Scissors/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Rock_Paper_Scissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
